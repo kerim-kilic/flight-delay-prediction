6698414d99e7a3217756a4f57ffdc16dfad94ccd
k-kilic <mail@kerim-kilic.com>
2022-10-12 16:18:58 +0200

    Refactor function to calculate metrics

--- src/functions.R
+++ src/functions.R
@@ -1,111 +1,89 @@
 generate_metrics_classification <- function(model,type,test_data)
 {
+  # Check if the data is from the test set or train set.
   if(type == "train")
   {
-  augment_value <- augment(model) %>% collect()
-  augment_value$delay <- as.factor(augment_value$delay)
-  augment_value$.predicted_label <- as.factor(augment_value$.predicted_label)
-  
-  model_accuracy <- augment_value %>%
-    accuracy(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  model_sensitivity <- augment_value %>%
-    sensitivity(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  model_specificity <- augment_value %>%
-    specificity(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  model_precision <- augment_value %>%
-    precision(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  model_recall <- augment_value %>%
-    recall(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  model_roc_auc <- augment_value %>%
-    mutate(.predicted_label = as.numeric(as.character(.predicted_label))) %>%
-    roc_auc(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  model_pr_auc <- augment_value %>%
-    mutate(.predicted_label = as.numeric(as.character(.predicted_label))) %>%
-    pr_auc(estimate = .predicted_label, truth = delay) %>%
-    pull(.estimate)
-  
-  metric_names <- c("Accuracy",
-                    "Sensitivity",
-                    "Specificity",
-                    "Precision",
-                    "Recall",
-                    "roc_auc",
-                    "pr_auc")
-  
-  model_metrics <- c(
-    model_accuracy,
-    model_sensitivity,
-    model_specificity,
-    model_precision,
-    model_recall,
-    model_roc_auc,
-    model_pr_auc)
-  
-  model_metric_results <-
-    data.frame(metric_names, model_metrics) %>%
-    mutate(model_metrics = round(model_metrics,3))
-  
-  colnames(model_metric_results) <- c("metric_names", "value")
+    predictions <- augment(model) %>%
+      mutate(predicted_label = .predicted_label) %>%
+      select(-.predicted_label)
+    ###
+    data <- augment(glm_model) %>%
+      mutate(prediction = as.numeric(.predicted_label),
+             label = as.numeric(delay)) %>%
+      select(-.predicted_label, -delay)
+    
+    model_pr_auc <- ml_metrics_binary(metrics = "pr_auc",
+                      x = data,
+                      estimate = prediction,
+                      truth = label) %>% 
+      pull(.estimate)
+    
+    model_roc_auc <- ml_metrics_binary(metrics = "roc_auc",
+                                       x = data,
+                                       estimate = prediction,
+                                       truth = label) %>% 
+      pull(.estimate)
   }
   if(type == "test")
   {
-    prediction <- ml_predict(model,test_data) 
-    model_accuracy <- prediction %>%
-      ml_metrics_multiclass(metrics = "accuracy") %>%
-      pull(.estimate)
-    
-    model_recall <- prediction %>%
-      ml_metrics_multiclass(metrics = "recall") %>%
-      pull(.estimate)
-    
-    model_precision <- prediction %>%
-      ml_metrics_multiclass(metrics = "precision") %>%
-      pull(.estimate)
-    
-    model_f1 <- prediction %>%
-      ml_metrics_multiclass(metrics = "f_meas") %>%
-      pull(.estimate)
-    
-    model_roc_auc <- prediction %>%
+    predictions <- ml_predict(model,test_data)
+    model_roc_auc <- predictions %>%
       ml_metrics_binary(metrics = "roc_auc") %>%
       pull(.estimate)
     
-    model_pr_auc <- prediction %>%
+    model_pr_auc <- predictions %>%
       ml_metrics_binary(metrics = "pr_auc") %>%
       pull(.estimate)
-    
-    metric_names <- c("Accuracy",
-                      "Recall",
-                      "precision",
-                      "f_meas",
-                      "roc_auc",
-                      "pr_auc")
-    
-    model_metrics <- c(
-      model_accuracy,
-      model_recall,
-      model_precision,
-      model_f1,
-      model_roc_auc,
-      model_pr_auc)
-    
-    model_metric_results <-
-      data.frame(metric_names, model_metrics) %>%
-      mutate(model_metrics = round(model_metrics,3))
-    
-    colnames(model_metric_results) <- c("metric_names", "value")
   }
+  
+  # Calculate the confusion matrix with TP, TN, FP, FN
+  TP <- sdf_nrow(predictions %>%
+                   filter(delay == "1", 
+                          predicted_label == "1"))
+  
+  TN <- sdf_nrow(predictions %>%
+                   filter(delay == "0", 
+                          predicted_label == "0"))
+  
+  FP <- sdf_nrow(predictions %>%
+                   filter(delay == "0", 
+                          predicted_label == "1"))
+  
+  FN <- sdf_nrow(predictions %>%
+                   filter(delay == "1", 
+                          predicted_label == "0"))
+  
+  # Calculate each model metric using confusion matrix
+  accuracy <- (TP + TN) / (sdf_nrow(predictions))
+  recall <- (TP) / (TP + FN)
+  precision <- (TP) / (TP + FP)
+  missclassification_rate <- (FP + FN) / (sdf_nrow(predictions))
+  specificity <- (TN) / (TN + FP)
+  f_score <- (2*precision*recall) / (precision+recall)
+  
+  metric_names <- c("accuracy",
+                    "recall",
+                    "precision",
+                    "missclassification_rate",
+                    "specificity",
+                    "f_score",
+                    "roc_auc",
+                    "pr_auc")
+
+  model_metrics <- c(
+    accuracy,    
+    recall,    
+    precision,    
+    missclassification_rate,
+    specificity,
+    f_score,
+    model_roc_auc,
+    model_pr_auc)
+      
+  model_metric_results <-
+    data.frame(metric_names, model_metrics) %>%
+    mutate(model_metrics = round(model_metrics,3))
+
+  colnames(model_metric_results) <- c("metric_names", "value")
   return(model_metric_results)
 }
