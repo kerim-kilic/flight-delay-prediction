e693d0941eaedd304630e31c07820f52360f4a7f
k-kilic <mail@kerim-kilic.com>
2023-01-11 18:03:03 +0100

    Updating train-validation-test split; Updating weather data; Adding confusion matrix generator; Adding comments

--- src/functions.R
+++ src/functions.R
@@ -2,7 +2,15 @@
 ### Author: Kerim Kiliç
 ###
 
+#####################################################################
 ### Function to generate and return metrics of machine learning model
+### model:      Sparklyr or H2O model to obtain metrics from.
+### type:       Metrics for the train or test data.
+###             For Sparklyr or H2O models
+### test_data:  Supply test data for metrics on test set
+### sc:         Spark connection
+### Returns the metrics on the train or test data.
+#####################################################################
 generate_metrics_classification <- function(model,type,test_data, sc)
 {
   # Check if the data is from the test set or train set.
@@ -157,81 +165,91 @@ generate_metrics_classification <- function(model,type,test_data, sc)
   return(model_metric_results)
 }
 
-# Function to create the split of the data into train and test
-create_train_test_split <- function(data, sample_size, ratio, type, hc)
+#####################################################################
+### Function to create train-test and train-validation-test split
+### data:       Input data to perform split on.
+### ratio:      Ratio to put into train data.
+###             Function derives test and validation ratio itself.
+### type:       "spark" for Sparklyr split
+###             "h2o" for H2O split
+### hc:         H2O connection
+### Returns the data split.
+#####################################################################
+create_train_test_split <- function(data, ratio, type, hc)
 {
-  ## Splitted datasets for classification
-  # Balanced training set using under sampling
-  train_size <- sample_size * ratio
-  test_size <- sample_size * (1-ratio)
-  
-  if(type == "numerical")
+  if(type == "spark")
   {
-    ## Splitted datasets for numerical prediction
-    data_tmp <- data %>%
-      sample_frac(sample_size/sdf_nrow(data))
-  
-    train_test_split <- sdf_random_split(x = data_tmp,
-                                            train_data = ratio,
-                                            test_data = 1-ratio,
-                                            seed = 1234)  
+    positive_samples <- data %>% filter(delay == "1")
+    negative_samples <- data %>% filter(delay == "0")
+    
+    positive_samples_split <- sdf_random_split(x = positive_samples,
+                                               train_data = ratio,
+                                               test_data = 1-ratio,
+                                               seed = 1234)
+    negative_samples_split <- sdf_random_split(x = negative_samples,
+                                               train_data = ratio,
+                                               test_data = 1-ratio,
+                                               seed = 1234)
+    negative_samp_t <- negative_samples_split$train_data
+    negative_samples_undersampled <- negative_samp_t %>%
+      sample_n(sdf_nrow(positive_samples_split$train_data))
+    
+    train_data <- rbind(positive_samples_split$train_data,
+                        negative_samples_undersampled)
+    
+    test_data <- rbind(positive_samples_split$test_data,
+                       negative_samples_split$test_data)
+    
+    train_test_split <- list(train_data = train_data,
+                             test_data = test_data)
   }
-  
-  if(type == "classification")
+  if(type == "h2o")
   {
-    delay_yes <- data %>%
-      filter(delay == "1")
-    delay_no <- data %>%
-      filter(delay == "0")
+    positive_samples <- data %>% filter(delay == "1")
+    negative_samples <- data %>% filter(delay == "0")
     
-    tmp1 <- delay_yes %>% 
-      sample_n(sample_size/2)
-    tmp2 <- delay_no %>% 
-      sample_n(sample_size/2)
-    data_tmp <- rbind(tmp1, tmp2)
+    positive_samples_split <- sdf_random_split(x = positive_samples,
+                                               train_data = ratio,
+                                               test_data = (1-ratio)/2,
+                                               valid_data = (1-ratio)/2,
+                                               seed = 1234)
+    negative_samples_split <- sdf_random_split(x = negative_samples,
+                                               train_data = ratio,
+                                               test_data = (1-ratio)/2,
+                                               valid_data = (1-ratio)/2,
+                                               seed = 1234)
+    negative_samp_t <- negative_samples_split$train_data
+    negative_samples_undersampled <- negative_samp_t %>%
+      sample_n(sdf_nrow(positive_samples_split$train_data))
     
-    train_test_split <- sdf_random_split(x = data_tmp,
-                                         train_data = ratio,
-                                         test_data = 1-ratio,
-                                         seed = 1234)  
+    train_data <- rbind(positive_samples_split$train_data,
+                        negative_samples_undersampled)
+    
+    valid_data <- rbind(positive_samples_split$valid_data,
+                        negative_samples_split$valid_data)
+    
+    test_data <- rbind(positive_samples_split$test_data,
+                       negative_samples_split$test_data)
+    
+    
+    train_test_split <- list(train_data = train_data,
+                             valid_data = valid_data,
+                             test_data = test_data)    
   }
-  
-  if(type == "h2o_classification")
-  {
-    delay_yes <- data %>%
-      filter(delay == "1")
-    delay_no <- data %>%
-      filter(delay == "0")
-    
-    tmp1 <- delay_yes %>% 
-      sample_n(sample_size/2)
-    tmp2 <- delay_no %>% 
-      sample_n(sample_size/2)
-    sample_data_classification <- rbind(tmp1, tmp2)
-    sample_data_classification <- hc$asH2OFrame(sample_data_classification)
-    
-    train_test_split <- h2o.splitFrame(
-      data = sample_data_classification,
-      ratios = c(ratio,(1-ratio)/2),   ## only need to specify 2 fractions, the 3rd is implied
-      destination_frames = c("train1.hex", "valid1.hex", "test1.hex"), seed = 1234
-    )
-  }
-  
-  if(type == "h2o_numerical")
-  {
-    sample_data_numerical <- data %>%
-      sample_n(sample_size)
-    sample_data_numerical <- hc$asH2OFrame(sample_data_numerical)
-    train_test_split <- h2o.splitFrame(
-      data = sample_data_numerical,
-      ratios = c(ratio,(1-ratio)/2),   ## only need to specify 2 fractions, the 3rd is implied
-      destination_frames = c("train2.hex", "valid2.hex", "test2.hex"), seed = 1234
-    )
-  }
-  
   return(train_test_split)
 }
 
+#####################################################################
+### Function to cross-validate Sparklyr models
+### sc:         Sparklyr connection.
+### data:       Training data used in cross-validation
+### pipeline:   Pipeline object to use in cross-validation.
+### grid:       Grid object to use in cross-validation.
+### type:       "classification" for classification models
+### folds:      Number of folds to perform cross-validation with.
+### seed:       Seed to create repeatability.
+### Returns cross-validation results.
+#####################################################################
 cross_validator <- function(sc, 
                             data,
                             pipeline,
@@ -241,10 +259,6 @@ cross_validator <- function(sc,
                             seed)
 {
 
-  if(type == "numerical") # how to evaluate the CV
-  {
-    evaluator <- ml_regression_evaluator(sc,metric_name = "r2")
-  }
   if(type == "classification")
   {
     evaluator <- ml_multiclass_classification_evaluator(sc,metric_name = "accuracy")
@@ -269,13 +283,7 @@ cross_validator <- function(sc,
   cv_results <- cv_model$avg_metrics_df
   if(type == "classification")
   {
-    # best_cv_result <- cv_results[which.max(cv_results$accuracy),"accuracy"]
     best_cv_result <- cv_results[which.max(cv_results$accuracy),]
-  }
-  if(type == "numerical")
-  {
-    # best_cv_result <- cv_results[which.max(cv_results$r2),"r2"] 
-    best_cv_result <- cv_results[which.max(cv_results$r2),] 
   }
    
   final_cv_results <- list(all_results = cv_results,
@@ -286,68 +294,139 @@ cross_validator <- function(sc,
   return(final_cv_results)
 }
 
-### Function to get the weather data from the top 10 airports
-
+#####################################################################
+### Function to process and prepare weather data.
+### Function saves processed data locally.
+### sc:         Sparklyr connection.
+#####################################################################
 get_weather_data <- function(sc)
 {
   ### Get the correct data for each airport
-  ATL_data_raw <- importNOAA(code = "722190-13874", year = 2017) %>%
+  ATL_data_raw <- read.csv("./data/30320_ATL.csv") %>%
     mutate(origin = "ATL")
-  DEN_data_raw <- importNOAA(code = "725650-03017", year = 2017) %>%
+  DEN_data_raw <- read.csv("./data/80249_DEN.csv") %>%
     mutate(origin = "DEN")
-  ORD_data_raw <- importNOAA(code = "725300-94846", year = 2017) %>%
+  ORD_data_raw <- read.csv("./data/60666_ORD.csv") %>%
     mutate(origin = "ORD")
-  LAX_data_raw <- importNOAA(code = "722950-23174", year = 2017) %>%
+  LAX_data_raw <- read.csv("./data/90045_LAX.csv") %>%
     mutate(origin = "LAX")
-  DFW_data_raw <- importNOAA(code = "722590-03927", year = 2017) %>%
+  DFW_data_raw <- read.csv("./data/75261_DFW.csv") %>%
     mutate(origin = "DFW")
-  SFO_data_raw <- importNOAA(code = "724940-23234", year = 2017) %>%
+  SFO_data_raw <- read.csv("./data/94128_SFO.csv") %>%
     mutate(origin = "SFO")
-  PHX_data_raw <- importNOAA(code = "722780-23183", year = 2017) %>%
+  PHX_data_raw <- read.csv("./data/85034_PHX.csv") %>%
     mutate(origin = "PHX")
-  LAS_data_raw <- importNOAA(code = "723860-23169", year = 2017) %>%
+  LAS_data_raw <- read.csv("./data/89119_LAS.csv") %>%
     mutate(origin = "LAS")
-  SEA_data_raw <- importNOAA(code = "727930-24233", year = 2017) %>%
+  SEA_data_raw <- read.csv("./data/98158_SEA.csv") %>%
     mutate(origin = "SEA")
-  MSP_data_raw <- importNOAA(code = "726580-14922", year = 2017) %>%
+  MSP_data_raw <- read.csv("./data/55111_MSP.csv") %>%
     mutate(origin = "MSP")
+  
   ### Create a single dataframe
   weather_data <- rbind(ATL_data_raw,DEN_data_raw,ORD_data_raw,LAX_data_raw,
                         DFW_data_raw,SFO_data_raw,PHX_data_raw,LAS_data_raw,
                         SEA_data_raw,MSP_data_raw)
   ### Define data pipeline for weather data
-  weather_pipeline <-  .%>%
-    mutate(date = as.Date(date,format = "%y-%m-%d")) %>%
-    select(origin,date,
-           # latitude,longitude,elev,
-           precip_6,
-           ws,wd,air_temp,atmos_pres,visibility,dew_point) 
+  weather_pipeline <- .%>%
+    clean_names() %>%
+    select(origin,
+           date_time,
+           winddir_degree,
+           windspeed_kmph,
+           visibility,
+           precip_mm,
+           cloudcover,
+           total_snow_cm,
+           pressure,
+           temp_c,
+           dew_point_c,
+           wind_gust_kmph)
   
   
   ### Initial weather dataset
   weather_data <- weather_pipeline(weather_data)
-  weather_data <- copy_to(dest = sc,
-                          df = weather_data,
-                          overwrite = TRUE)
-  ### Perform imputation on missing values
-  input_cols_imp <- c("precip_6","ws","wd","air_temp","atmos_pres","visibility","dew_point")
-  output_cols_imp <- paste0(input_cols_imp,"_imp")
-  weather_data <- ft_imputer(weather_data, input_cols = input_cols_imp, output_cols = output_cols_imp, strategy = "mean")
-  
-  ### Calculate average value per day
-  weather_data <- weather_data %>% collect()
-  weather_data <- weather_data %>%
-    group_by(origin, date) %>%
-    summarise(across(c(ws_imp,
-                       # latitude, longitude, elev, 
-                       wd_imp, air_temp_imp, atmos_pres_imp,
-                       visibility_imp, dew_point_imp,precip_6_imp), mean))
-  
-  ### One dataframe for the origin weather data and one for the destination weather data
+  colnames(weather_data) <- c("origin", 
+                              "date", 
+                              "origin_wd", 
+                              "origin_ws", 
+                              "origin_visibility", 
+                              "origin_precip", 
+                              "origin_cloudcover", 
+                              "origin_total_snow", 
+                              "origin_atmos_pressure",
+                              "origin_air_temp",
+                              "origin_dew_point",
+                              "origin_wind_gust")
   weather_data2 <- weather_data
-  colnames(weather_data) <- c("origin", "date", "origin_ws", "origin_wd", "origin_air_temp", "origin_atmos_pres", "origin_visibility", "origin_dew_point", "origin_precip_6")
-  colnames(weather_data2) <- c("destination", "date", "destination_ws", "destination_wd", 
-                               "destination_air_temp", "destination_atmos_pres", "destination_visibility", "destination_dew_point","destination_precip_6")
-  fwrite(weather_data,file = "data/origin_weather_data.csv") 
-  fwrite(weather_data2,file = "data/destination_weather_data.csv")
+  colnames(weather_data2) <- c("destination", 
+                               "date", 
+                               "destination_wd", 
+                               "destination_ws", 
+                               "destination_visibility", 
+                               "destination_precip", 
+                               "destination_cloudcover", 
+                               "destination_total_snow", 
+                               "destination_atmos_pressure",
+                               "destination_air_temp",
+                               "destination_dew_point",
+                               "destination_wind_gust")
+  
+  fwrite(weather_data,file = "data/new_weather_data/origin_weather_data.csv") 
+  fwrite(weather_data2,file = "data/new_weather_data/destination_weather_data.csv")
+}
+
+#####################################################################
+### Function to obtain confusion matrix elements for test data.
+### model:        Sparklyr or H2O model object.
+### test_data:    Test data to obtain confusion matrix from.
+### type:         "spark" for Sparklyr model, "h2o" for H2O model.
+### Returns confusion matrix elements. 
+#####################################################################
+confusion_matrix_elements <- function(model,test_data,type){
+  if(type == "spark")
+  {
+    predictions <- ml_predict(model,test_data)  
+  }
+  if(type == "h2o")
+  {
+    predictions <- h2o.predict(model, test_data)
+    predictions <- as.data.frame(predictions)
+    actual_delays <- data.frame(as.data.frame(test_data) %>% pull(delay))
+    colnames(actual_delays) <- c("actual_delay")
+    
+    evaluation_frame <- predictions %>%
+      append(actual_delays)
+    
+    predictions <- evaluation_frame
+    predictions <- data.frame(predictions) %>%
+      select(predict,actual_delay)
+    
+    colnames(predictions) <- c("predicted_label", "delay")
+    
+    predictions <- copy_to(dest = sc,
+                           df = predictions,
+                           overwrite = TRUE)
+  }
+  
+  TP <- sdf_nrow(predictions %>%
+                   filter(delay == "1",
+                          predicted_label == "1"))
+  
+  TN <- sdf_nrow(predictions %>%
+                   filter(delay == "0",
+                          predicted_label == "0"))
+  
+  FP <- sdf_nrow(predictions %>%
+                   filter(delay == "0",
+                          predicted_label == "1"))
+  
+  FN <- sdf_nrow(predictions %>%
+                   filter(delay == "1",
+                          predicted_label == "0"))
+  confusion_matrix_element <- list(TP = TP,
+                                   TN = TN,
+                                   FP = FP,
+                                   FN = FN)
+  return(confusion_matrix_element)
 }
